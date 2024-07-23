0f4210ae19592ad2a345ac7652fee98cedbc2f97
kibizkat <131921494+kibizkat@users.noreply.github.com>
2024-07-23 17:34:59 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,68 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("nav a");
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a');
+    const sections = document.querySelectorAll('section');
 
-    function showSection(id) {
-        sections.forEach(section => {
-            if (section.id === id) {
-                section.classList.add("active");
-            } else {
-                section.classList.remove("active");
-            }
-        });
-    }
-
-    navLinks.forEach(link => {
-        link.addEventListener("click", function(event) {
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
             event.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            history.pushState(null, null, `#${targetId}`);
-            showSection(targetId);
+            const targetId = event.currentTarget.getAttribute('href').substring(1);
+
+            sections.forEach(section => {
+                if (section.id === targetId) {
+                    section.classList.add('active');
+                    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                } else {
+                    section.classList.remove('active');
+                }
+            });
         });
     });
-
-    const initialHash = window.location.hash.substring(1);
-    if (initialHash) {
-        showSection(initialHash);
-    } else {
-        sections[0].classList.add("active");
-    }
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    const stripe = Stripe('pk_live_51PdxfURxV6IwpmDrha2SKIRsy3kb0Usuk3oYHyW1ldE0wUrEibSCMgNgvUbgoRQ6gQOJXCnqbxyrMUiyZJhR2zpb00aKxpYWHC'); // Substitua 'YOUR_PUBLISHABLE_KEY' pela sua chave pública do Stripe
-    const comprarAdultoBtn = document.getElementById('comprar-adulto');
-    const comprarCriancaBtn = document.getElementById('comprar-crianca');
-
-    comprarAdultoBtn.addEventListener('click', function() {
-        handleCheckout(120); // Preço em centavos (R$ 1,20)
-    });
-
-    comprarCriancaBtn.addEventListener('click', function() {
-        handleCheckout(80); // Preço em centavos (R$ ,80)
-    });
-
-    function handleCheckout(amount) {
-        stripe.redirectToCheckout({
-            lineItems: [{
-                price_data: {
-                    currency: 'brl',
-                    product_data: {
-                        name: 'Ingresso',
-                    },
-                    unit_amount: amount,
-                },
-                quantity: 1,
-            }],
-            mode: 'payment',
-            successUrl: window.location.origin + '/success.html',
-            cancelUrl: window.location.origin + '/cancel.html',
-        })
-        .then(function (result) {
-            if (result.error) {
-                alert(result.error.message);
-            }
-        });
-    }
-});
-
